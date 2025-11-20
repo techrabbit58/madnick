801e881456ed1d370f843e6315176ef4114858d2
techrabbit58 <dzb@pruefbit.de>
2025-11-20 18:09:14 +0100

    lmc.py: trace subcommand is w.i.p. (just started)

--- src/lmc/lmc.py
+++ src/lmc/lmc.py
@@ -3,6 +3,14 @@ from typing import Annotated
 
 import rich
 import typer
+from rich.syntax import Syntax
+from rich.text import Text
+from textual.app import App, ComposeResult, RenderResult
+from textual.containers import ScrollableContainer
+from textual.binding import Binding
+from textual.reactive import reactive
+from textual.widget import Widget
+from textual.widgets import Header, Footer, Static
 
 from .assembler import Assembler
 from .vm import LMC, int_reader, IntWriter
@@ -22,6 +30,7 @@ def run(
             min=-500, max=499, help="Run with a list of input numbers")] = None,
         signed: Annotated[bool, typer.Option(
             "--signed/--unsigne", help="Signed output (default: unsigned numbers)")] = False) -> None:
+
     asm = Assembler()
     code = asm.run(prog.read_text())
     vm = LMC()
@@ -36,9 +45,54 @@ def run(
         rich.print("OK", output)
 
 
+class CodeView(Widget):
+    """Widget to display Python code."""
+
+    code = reactive("")
+
+    def render(self) -> RenderResult:
+        # Syntax is a Rich renderable that displays syntax highlighted code
+        syntax = Syntax(self.code, "c-objdump", line_numbers=True, indent_guides=True)
+        return syntax
+
+
+class ScreenApp(App):
+    TITLE = "LMC"
+    SUB_TITLE = "The Little Man Computer"
+
+    BINDINGS = [
+        Binding("s", "step", "Step"),
+        Binding("q", "quit", "Quit", priority=True),
+    ]
+
+    DEFAULT_CSS = """
+    .box { width: 1fr; }
+    """
+
+    def __init__(self, prog: Path, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.vm = LMC()
+        asm = Assembler()
+        code = asm.run(prog.read_text())
+        self.vm.load(code)
+        self.reg_view = Static(Text(str(self.vm)), classes="box")
+
+    def action_step(self) -> None:
+        self.vm.single_step()
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield ScrollableContainer(
+            self.reg_view
+        )
+        yield Footer()
+
+
 @app.command(help="Assemble, load and interactively step through a given LMC program")
 def trace(prog: Path) -> None:
-    rich.print(f"[bold yellow]trace[/] [cyan]{prog}[/] visually ...")
+    visual_app = ScreenApp(prog)
+    visual_app.sub_title = str(prog.absolute())
+    visual_app.run()
 
 
 if __name__ == "__main__":
